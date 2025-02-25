d838ae124f53528f1101e553eb4f66b4fc866585
chanchal844 <125867982+chanchal844@users.noreply.github.com>
2025-02-25 09:27:12 +0530

    Add files via upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,11 @@
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
+
 $('.responsive').slick({
     dots: false,
     infinite: false,
